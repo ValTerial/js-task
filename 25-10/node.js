fed5007a7d4491858befbbba9a4261d56181fedc
valentine <hohpoh5@gmail.com>
2022-10-25 10:42:33 +0600

    feat: 25-10-task

---
+++ 25-10/node.js
@@ -0,0 +1,18 @@
+/*1)*/
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+alert(result);
+/*2)*/
+let hour = 10;
+let minute = 32;
+let second = 10;
+alert(`${hour}:${minute}:${second}`);
+/*3)*/
+var num = 10;
+num = ++num;
+num = ++num;
+alert(num);
+
