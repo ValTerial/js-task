65bafba1673ce7e09100ce85a6683e88297212bb
valentine <hohpoh5@gmail.com>
2022-10-26 10:59:34 +0600

    feat: 26-10

---
+++ 26-10/node.js
@@ -0,0 +1,50 @@
+/****** && (u) *******/
+
+/*
+let userName = 'Igor';
+let userAge = 26;
+let userCompany = 'GazProm';
+let result = userAge > 27 && userCompany === 'GazProm';       //false + true = false
+let result = userName === 'Egor' && userAge == 43;            //false + false = false
+let result = userCompany === 'GazProm' && userName === 'Igor';//true + true = true
+console.log(result);*/
+
+
+/****** || (или) *******/
+
+/*let user_age = 17;
+let user_name = 'sasha';
+let result = user_age >= 17 || user_name === 'test';//true + false = true
+let result = user_age > 18 || user_name === 'sasha'; //false + true = false
+let result = user_age < 16 || user_name === 'sasha';//true + true = true
+console.log(result);*/
+
+
+/*1)
+let num = 10;
+++num;
+++num;
+--num;
+console.log(num);*/
+
+/*2)
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);*/
+
+
+/*3)
+let num = 1;
+num = num += 12;
+num = num -= 14;
+num = num *= 5;
+num = num /= 7;
+++num;
+--num;
+alert(num);*/
+
+
+
