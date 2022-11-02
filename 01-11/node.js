38efaf652d1201db4a066078247b90dc3905e2aa
ValTerial <116243315+ValTerial@users.noreply.github.com>
2022-11-02 09:07:27 +0600

    Update node.js

--- 01-11/node.js
+++ 01-11/node.js
@@ -1,28 +1,28 @@
 //1)
 
-/*const numberCode = [1, 2, 5, 6, 88, 5];
+/*const numbersCode = [1, 2, 5, 6, 88, 5];
 let result = 0;
-for (let i = 0; i < numberCode.length; i++){
-    result += numberCode[i];
+for (let i = 0; i < numbersCode.length; i++){
+    result += numbesrCode[i];
 }
 console.log(result);*/
 
 
 //2
 
-/*const numberCode = [11, 33, 12, 1, 4, 14, 9, 77, 62, 18];
-let result = 0;
-for (let i = 0; i < numberCode.length; i++){
-    result = result += numberCode[i]**2;
+/*const numbersCode = [11, 33, 12, 1, 4, 14, 9, 77, 62, 18];
+let result = [];
+for (let i = 0; i < numbesrCode.length; i++){
+    result += numbersCode[i]**2;
 }
 console.log(result);*/
 
 //3
 
 /*const numberCode = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-let result = 0;
+let result = [];
 for (let i = 0; i <= numberCode.length; i++){
-    if (i % 2 == 0){
+    if (i % 2 === 0){
         result += numberCode[i];
     }
 }
