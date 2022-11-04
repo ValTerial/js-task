b9b14c6e26918c6f276a53774d221ad457fbac8d
valentine <hohpoh5@gmail.com>
2022-11-04 08:53:36 +0600

    feat 03-11

---
+++ 03-11/node.js
@@ -0,0 +1,33 @@
+//1)
+
+/*const  example = [1, 2, 3];
+const doubleNumbers = example.map(item => item * 2);
+console.log(doubleNumbers);
+
+
+const example2 = [4, 1, 1, 1, 4];
+const doubleNumbers2 = example2.map(item => item * 2);
+console.log(doubleNumbers2);
+
+
+const example3 = [2, 2, 2, 2, 2, 2];
+const doubleNumbers3 = example3.map(item => item * 2);
+console.log(doubleNumbers3);*/
+
+
+//2)
+
+
+
+
+//3)
+/*const data = [1, 2, 3];
+function myFunc2(data) {
+    res = [];
+    for (let i = data.length - 1; i >= 0; i--){
+        res.push(data[i]);
+    }
+    return res;
+}
+console.log(myFunc2(data))*/
+
