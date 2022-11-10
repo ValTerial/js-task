86f96a0ebccd3cc8f77955c881d74923bc4d8a57
valentine <hohpoh5@gmail.com>
2022-11-10 09:18:04 +0600

    feat: 09-11

--- 09-11/style.js
+++ 09-11/style.js
@@ -1,16 +1,12 @@
 //1)
-/*const timer1 = func1 => alert(func1);
+/*const timer1 = string1 => alert(func1);
 setTimeout(timer1, 4000, 'Hello after');
 
-const timer2 = func2 => alert(func2);
+const timer2 = string2 => alert(func2);
 setTimeout(timer2, 8000, 'Hello after');*/
 
 /*
-2) Составьте программу, которая принимает с клавиатуры два целых числа,
-    первое из которых - год рождения ученика, а второе – нынешний (текущий) год.
-    Программа должна вывести на экран возраст ученика (в целых годах).
-ввод: 2006, 2018 ⇒ вывод: вам 12 лет)*/
-
+//2)
 const yearOfBirth = +prompt('год рождения');
 const tishYear = +prompt('нынешний год')
 let result = yearOfBirth - tishYear;
