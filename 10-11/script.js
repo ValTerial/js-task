36db4fec0fcb3287ea90e323f04765533babe15d
valentine <hohpoh5@gmail.com>
2022-11-11 09:01:31 +0600

    feat: 10-11

---
+++ 10-11/script.js
@@ -0,0 +1,35 @@
+//1)
+const picture_loss = document.querySelector('#loss').onclick = () => {
+    document.querySelector('.desc_loss').innerHTML = 'dollar loss!!!'
+}
+
+//2)
+const info_loss = document.querySelector('#info_loss').onmouseenter = () => {
+    alert("INFO loss");
+}
+
+
+//1)
+const picture_gain = document.querySelector('#gain').onclick = () => {
+    document.querySelector('.desc_gain').innerHTML = 'dollar gain!!!'
+}
+
+//2)
+const info_gain = document.querySelector('#info_gain').onmouseenter = () => {
+    alert("INFO gain");
+}
+
+//3)
+let stringArrayH = ['Hello'];
+let stringArrayW = ['world'];
+const sum = () =>{
+    return
+}
+
+
+
+
+
+
+
+
